1dd0ebec802f174d57f110d56d2fb3610c39309d
May Moftah <mmoftah@uncc.edu>
2016-04-19 11:03:17 -0400

    Added Division + Multiplication
    Reformatted output to proper outputting
    Documented code
    
    Signed-off-by: May Moftah <mmoftah@uncc.edu>

--- calculator_implementation.adb
+++ calculator_implementation.adb
@@ -11,10 +11,13 @@ procedure calculator_implementation is
 	Power : Integer;
 	Exponent : Integer;
 	NumberValues : Natural;
+	Value_A : Float;
+	Value_B : Float;
+	Answer_B : Float;
 	
 	SUBTYPE NonNegFloat IS Float RANGE 0.0 .. Float'Last;
-	Value : NonNegFloat;
-	Answer : NonNegFloat;
+		Value : NonNegFloat;
+		Answer : NonNegFloat;
 
 
 		begin 
@@ -58,6 +61,7 @@ procedure calculator_implementation is
 					Sum := 0;
 					Put("Enter the integer values to be summed up, separated by a space: ");
 
+					--for loop takes in multiple integers and adds recursively
 					for Count in 1 .. NumberValues loop
 						get (Item => CurrentValue);
 						Sum := Sum + CurrentValue;
@@ -70,6 +74,8 @@ procedure calculator_implementation is
 
 					Sum := 0;
 					Put("Enter the integer values to be subtracted, separated by a space: ");
+					
+					--subtracts 2 values 
 					for Count in 1 .. 2 loop
 						get (Item => CurrentValue);
 						Sum:= CurrentValue - Sum;
@@ -77,46 +83,80 @@ procedure calculator_implementation is
 
 					Put(Item => "The result of subtracting your values is: " & Sum'img);
 					
-			-- Performing Squaring operation	
+			-- Performing Multiplication Operation
+				when 3 =>
+					
+					Ada.Text_IO.Put(Item => "In the form of a x b, enter the value a: ");
+					Ada.Float_Text_IO.get(Item => Value_A);
+					Ada.Text_IO.Put(Item => "In the form of a x b, enter the value b: ");
+					Ada.Float_Text_IO.get(Item => Value_B);
+					Answer_B := Value_A * Value_B;
+
+						
+					Ada.Text_IO.Put("The result of your multiplication is: ");
+					Ada.Float_Text_IO.Put (Item => Answer_B, Fore => 1, Aft => 4, Exp => 0);
+			
+
+					
+			-- Performing Division Operation
+				when 4 =>
+					Ada.Text_IO.Put(Item => "In the form of b / a, enter the the value b: ");
+					Ada.Float_Text_IO.get(Item => Value_B);
+					Ada.Text_IO.Put(Item => "In the form of b / a, enter the the value a: ");
+					Ada.Float_Text_IO.get(Item => Value_A);
+					Answer_B := Value_B / Value_A;
+					
+					Ada.Text_IO.Put("The result of your division is: ");
+					
+					-- formats decimal output with 4 decimal point
+					Ada.Float_Text_IO.Put (Item => Answer_B, Fore => 1, Aft => 4, Exp => 0);
+
+					
+			-- Performing Square Operation
 				when 5 =>
 					Put("Enter the value to be squared: ");
 					get (Item => CurrentValue);
 					SquareValue := CurrentValue * CurrentValue;
 					Put("The square value of the entered number is: " & SquareValue'img);
 					
-			-- Perform Square Root operation
+					
+			-- Performing Square Root operation
 				when 6 =>
 					Ada.Text_IO.Put_Line("The following operation will find and output the square root of your desired value");
 					Ada.Text_IO.Put(Item => "In the form of sqrt(x), enter the value x which you wish to square root: ");
 					Ada.Float_Text_IO.get(Item => Value);
 					Answer := Ada.Numerics.Elementary_Functions.Sqrt(Value);
 					Ada.Text_IO.Put("The square root of your input is: ");
-					Ada.Float_Text_IO.Put (Item => Answer, Fore => 1, Aft => 5, Exp => 0);
+					Ada.Float_Text_IO.Put (Item => Answer, Fore => 1, Aft => 4, Exp => 0);
 					Ada.Text_IO.New_Line;
 
 					
-			-- Perform Exponential operation
+			-- Performing Exponential operation
 				when 7 => 
 					Put("In the form of exponentiation, a^N. Enter the integer value, a: ");
 					get(Item => CurrentValue);
 					Put("In the form of exponentation, a^N. Enter the integer value, N: ");
 					get (Item => Power);
+					
+					-- performs built-in exponentiation function
 					Exponent := CurrentValue ** Power;
 					Put("Your exponential value is: " & Exponent'img);
 					New_Line(1);
 					
-			-- Perform Logarithmic operation
+					
+			-- Perfoming Log operation
 				when 8 =>
 					Ada.Text_IO.Put_Line("The following operation will perform Log10.");
 					Ada.Text_IO.Put(Item => "In the form of lg(x), where the log base 10 of x will be found, enter the value x: ");
 					Ada.Float_Text_IO.get(Item => Value);
+					
+					--uses Ada library to perform built-in log (x, base) function/ method
 					Answer := Ada.Numerics.Elementary_Functions.Log(Value, 10.0);
 					Ada.Text_IO.Put("The logarithmic value of your input is: ");
 					Ada.Float_Text_IO.Put (Item => Answer, Fore => 1, Aft => 5, Exp => 0);
 					Ada.Text_IO.New_Line;
 					
-			
-									
+						
 			-- Exit Program		
 				when 0 => 
 					Put_Line("Goodbye!");
@@ -131,7 +171,7 @@ procedure calculator_implementation is
 				
 				
 				
-end calculator_implementation;
+end calculator_implementation; --end procedure
 			
 			
 			
